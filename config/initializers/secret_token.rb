485c72e9c2eb35099e321bdea27bf8b88e88ec23
nilakanta <nilakanta@gmail.com>
2013-09-10 08:57:23 +0530

    initial skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReportableXls::Application.config.secret_key_base = '419a810727402f2097292b19456810bd289e720bff0031f4665ab56e888c88e4c5a5899821e8560199a55fb1eb9dee9590aa65f85b3d34de901b7783bf641537'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReportableXls::Application.config.session_store :cookie_store, key: '_reportable-xls_session'
